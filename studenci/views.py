85e4f5b6200d98b3bd2841aa1fea94ae7bf9c2e3
lo1cgsan <lo1cgsan@gmail.com>
2019-10-17 13:52:58 +0200

    przetwarzanie formularza dodawania miasta cz. 1

--- studenci/views.py
+++ studenci/views.py
@@ -11,6 +11,12 @@ def index(request):
 
 def miasta(request):
 
+    if request.method == 'POST':
+        nazwa = request.POST.get('nazwa')
+        kod = request.POST.get('kod')
+        m = Miasto(nazwa=nazwa, kod=kod)
+        m.save()
+
     miasta = Miasto.objects.all()
     kontekst = {
         'miasta': miasta
