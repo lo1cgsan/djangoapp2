cd63aaf591c79dc47b4ec1098e8594fcff28394f
lo1cgsan <lo1cgsan@gmail.com>
2019-11-28 14:21:34 +0100

    rejestracja i logowanie użytkowników

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ app_name = 'users'
 urlpatterns = [
 	path('', views.index, name='index'),
     path('rejestruj/', views.rejestruj, name='rejestruj'),
+    path('loguj/', views.loguj, name='loguj'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from users.forms import UserLoginForm
+
 
 def index(request):
     pass
@@ -7,9 +12,33 @@ def index(request):
 
 def rejestruj(request):
     if request.method == 'POST':
-        pass
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Konto utworzono!")
+            return redirect(reverse('users:index'))
     else:
         form = UserCreationForm()
 
     kontekst = {'form': form}
     return render(request, 'users/rejestruj.html', kontekst)
+
+
+def loguj(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            nazwa = form.cleaned_data['login']
+            haslo = form.cleaned_data['haslo']
+            user = authenticate(request, username=nazwa, password=haslo)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Zostałeś zalogowany!")
+                return redirect(reverse('studenci:index'))
+            else:
+                messages.error(request, "Błędny login lub hasło!")
+    else:
+        form = UserLoginForm()
+
+    kontekst = {'form': form}
+    return render(request, 'users/loguj.html', kontekst)
