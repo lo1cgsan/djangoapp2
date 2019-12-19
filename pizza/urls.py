d927f28e0120422eed0d6e0a0d6e92767dee5c09
lo1cgsan <lo1cgsan@gmail.com>
2019-12-19 14:22:10 +0100

    formularz składnika oparty na modelu

--- pizza/urls.py
+++ pizza/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'pizza'
 urlpatterns = [
 	path('', views.index, name='index'),
-    path('news/', views.news, name='news'),
+    path('skladnik/dodaj', views.SkladnikDodaj, name='skladnik_add'),
 ]
